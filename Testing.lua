520e05ec89a78355f2f02844315b8633e6aa4a40
Ayden Serna <114900435+nmsjayden@users.noreply.github.com>
2024-12-03 13:13:42 -0600

    Update Testing.lua

--- Testing.lua
+++ Testing.lua
@@ -1,161 +1,260 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
-local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
-local Wait = library.subs.Wait -- Only returns if the GUI has not been terminated. For 'while Wait() do' loops
+local Window = Fluent:CreateWindow({
+    Title = "Fluent " .. Fluent.Version,
+    SubTitle = "by dawid",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
 
-local PepsisWorld = library:CreateWindow({
-Name = "Pepsi's World",
-Themeable = {
-Info = "Discord Server: VzYTJ7Y"
+--Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
+
+local Options = Fluent.Options
+
+do
+    Fluent:Notify({
+        Title = "Notification",
+        Content = "This is a notification",
+        SubContent = "SubContent", -- Optional
+        Duration = 5 -- Set to nil to make the notification not disappear
+    })
+
+
+
+    Tabs.Main:AddParagraph({
+        Title = "Paragraph",
+        Content = "This is a paragraph.\nSecond line!"
+    })
+
+
+
+    Tabs.Main:AddButton({
+        Title = "Button",
+        Description = "Very important button",
+        Callback = function()
+            Window:Dialog({
+                Title = "Title",
+                Content = "This is a dialog",
+                Buttons = {
+                    {
+                        Title = "Confirm",
+                        Callback = function()
+                            print("Confirmed the dialog.")
+                        end
+                    },
+                    {
+                        Title = "Cancel",
+                        Callback = function()
+                            print("Cancelled the dialog.")
+                        end
+                    }
+                }
+            })
+        end
+    })
+
+
+
+    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
+
+    Toggle:OnChanged(function()
+        print("Toggle changed:", Options.MyToggle.Value)
+    end)
+
+    Options.MyToggle:SetValue(false)
+
+
+    
+    local Slider = Tabs.Main:AddSlider("Slider", {
+        Title = "Slider",
+        Description = "This is a slider",
+        Default = 2,
+        Min = 0,
+        Max = 5,
+        Rounding = 1,
+        Callback = function(Value)
+            print("Slider was changed:", Value)
+        end
+    })
+
+    Slider:OnChanged(function(Value)
+        print("Slider changed:", Value)
+    end)
+
+    Slider:SetValue(3)
+
+
+
+    local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
+        Title = "Dropdown",
+        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+        Multi = false,
+        Default = 1,
+    })
+
+    Dropdown:SetValue("four")
+
+    Dropdown:OnChanged(function(Value)
+        print("Dropdown changed:", Value)
+    end)
+
+
+    
+    local MultiDropdown = Tabs.Main:AddDropdown("MultiDropdown", {
+        Title = "Dropdown",
+        Description = "You can select multiple values.",
+        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+        Multi = true,
+        Default = {"seven", "twelve"},
+    })
+
+    MultiDropdown:SetValue({
+        three = true,
+        five = true,
+        seven = false
+    })
+
+    MultiDropdown:OnChanged(function(Value)
+        local Values = {}
+        for Value, State in next, Value do
+            table.insert(Values, Value)
+        end
+        print("Mutlidropdown changed:", table.concat(Values, ", "))
+    end)
+
+
+
+    local Colorpicker = Tabs.Main:AddColorpicker("Colorpicker", {
+        Title = "Colorpicker",
+        Default = Color3.fromRGB(96, 205, 255)
+    })
+
+    Colorpicker:OnChanged(function()
+        print("Colorpicker changed:", Colorpicker.Value)
+    end)
+    
+    Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
+
+
+
+    local TColorpicker = Tabs.Main:AddColorpicker("TransparencyColorpicker", {
+        Title = "Colorpicker",
+        Description = "but you can change the transparency.",
+        Transparency = 0,
+        Default = Color3.fromRGB(96, 205, 255)
+    })
+
+    TColorpicker:OnChanged(function()
+        print(
+            "TColorpicker changed:", TColorpicker.Value,
+            "Transparency:", TColorpicker.Transparency
+        )
+    end)
+
+
+
+    local Keybind = Tabs.Main:AddKeybind("Keybind", {
+        Title = "KeyBind",
+        Mode = "Toggle", -- Always, Toggle, Hold
+        Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
+
+        -- Occurs when the keybind is clicked, Value is `true`/`false`
+        Callback = function(Value)
+            print("Keybind clicked!", Value)
+        end,
+
+        -- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
+        ChangedCallback = function(New)
+            print("Keybind changed!", New)
+        end
+    })
+
+    -- OnClick is only fired when you press the keybind and the mode is Toggle
+    -- Otherwise, you will have to use Keybind:GetState()
+    Keybind:OnClick(function()
+        print("Keybind clicked:", Keybind:GetState())
+    end)
+
+    Keybind:OnChanged(function()
+        print("Keybind changed:", Keybind.Value)
+    end)
+
+    task.spawn(function()
+        while true do
+            wait(1)
+
+            -- example for checking if a keybind is being pressed
+            local state = Keybind:GetState()
+            if state then
+                print("Keybind is being held down")
+            end
+
+            if Fluent.Unloaded then break end
+        end
+    end)
+
+    Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
+
+
+    local Input = Tabs.Main:AddInput("Input", {
+        Title = "Input",
+        Default = "Default",
+        Placeholder = "Placeholder",
+        Numeric = false, -- Only allows numbers
+        Finished = false, -- Only calls callback when you press enter
+        Callback = function(Value)
+            print("Input changed:", Value)
+        end
+    })
+
+    Input:OnChanged(function()
+        print("Input updated:", Input.Value)
+    end)
+end
+
+
+-- Addons:
+-- SaveManager (Allows you to have a configuration system)
+-- InterfaceManager (Allows you to have a interface managment system)
+
+-- Hand the library over to our managers
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+SaveManager:IgnoreThemeSettings()
+
+-- You can add indexes of elements the save manager should ignore
+SaveManager:SetIgnoreIndexes({})
+
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+
+
+Window:SelectTab(1)
+
+Fluent:Notify({
+    Title = "Fluent",
+    Content = "The script has been loaded.",
+    Duration = 8
 })
 
-local GeneralTab = PepsisWorld:CreateTab({
-Name = "General"
-})
-local FarmingSection = GeneralTab:CreateSection({
-Name = "Farming"
-})
-FarmingSection:AddToggle({
-Name = "EXP Grinder",
-Flag = "FarmingSection_EXPGrinder"
-})
-FarmingSection:AddToggle({
-Name = "Trick Spammer",
-Flag = "FarmingSection_TrickSpammer",
-Keybind = 1,
-Callback = print
-})
-FarmingSection:AddSlider({
-Name = "Trick Rate",
-Flag = "FarmingSection_TrickRate",
-Value = 0.15,
-Precise = 2,
-Min = 0,
-Max = 1
-})
-FarmingSection:AddToggle({
-Name = "TP To Coins",
-Flag = "FarmingSection_TPCoins"
-})
-FarmingSection:AddToggle({
-Name = "Collect Coins",
-Flag = "FarmingSection_CollectCoins",
-Callback = print
-})
-FarmingSection:AddSlider({
-Name = "Coin Distance",
-Flag = "FarmingSection_CoinDistance",
-Value = 175,
-Min = 0,
-Max = 200,
-Format = function(Value)
-if Value == 0 then
-return "Collection Distance: Infinite"
-else
-return "Collection Distance: " .. tostring(Value)
-end
-end
-})
-
-local BoardControlSection = GeneralTab:CreateSection({
-Name = "Board Control"
-})
-BoardControlSection:AddToggle({
-Name = "Anti Trip/Ragdoll",
-Flag = "BoardControlSection_AntiTripRagdoll",
-Callback = print
-})
-BoardControlSection:AddToggle({
-Name = "No Wear & Tear",
-Flag = "BoardControlSection_NoWearTear"
-})
-BoardControlSection:AddToggle({
-Name = "No Trick Cooldown",
-Flag = "BoardControlSection_NoTrickCooldown",
-Callback = print
-})
-BoardControlSection:AddToggle({
-Name = "Extend Combo Timout",
-Flag = "BoardControlSection_ExtendComboTimeout"
-})
-BoardControlSection:AddSlider({
-Name = "Timeout Extension",
-Flag = "BoardControlSection_CoinDistance",
-Value = 3,
-Min = 0,
-Max = 20,
-Format = function(Value)
-if Value == 0 then
-return "Combo Timeout: Never"
-else
-return "Combo Timeout: " .. tostring(Value) .. "s"
-end
-end
-})
-
-local MiscSection = GeneralTab:CreateSection({
-Name = "Misc",
-Side = "Right"
-})
-MiscSection:AddToggle({
-Name = "Unlock Gamepasses",
-Flag = "MiscSection_UnlockGamepasses",
-Callback = print
-})
-MiscSection:AddToggle({
-Name = "Auto Compete",
-Flag = "MiscSection_AutoCompete",
-Callback = print
-})
-MiscSection:AddButton({
-Name = "Repair Board",
-Callback = function()
-print("Fixed")
-end
-})
-MiscSection:AddKeybind({
-Name = "Test Key",
-Callback = print
-})
-MiscSection:AddToggle({
-Name = "Test Toggle/Key",
-Keybind = {
-Mode = "Dynamic" -- Dynamic means to use the 'hold' method, if the user keeps the button pressed for longer than 0.65 seconds; else use toggle method
-},
-Callback = print
-})
-
-local FunSection = GeneralTab:CreateSection({
-Name = "Fun Cosmetics"
-})
-FunSection:AddToggle({
-Name = "Ragdoll Assumes Flight",
-Flag = "FunSection_AssumesFlight"
-})
-FunSection:AddToggle({
-Name = "Ragdoll On Player Collision",
-Flag = "FunSection_RagdollOnPlayerCollision"
-})
-FunSection:AddToggle({
-Name = "Un-Ragdoll When Motionless",
-Flag = "FunSection_UnRagdollWhenMotionless"
-})
-FunSection:AddToggle({
-Name = "Extend Ragdoll Duration",
-Flag = "FunSection_ExtendRagdollDuration"
-})
-FunSection:AddSlider({
-Name = "Coin Distance",
-Flag = "FarmingSection_Coin Distance",
-Value = 4,
-Min = 0,
-Max = 60,
-Textbox = true,
-Format = function(Value)
-if Value == 0 then
-return "Ragdoll Extension: Indefinite"
-else
-return "Ragdoll Extension: " .. tostring(Value) .. "s"
-end
-end
-})
+-- You can use the SaveManager:LoadAutoloadConfig() to load a config
+-- which has been marked to be one that auto loads!
+SaveManager:LoadAutoloadConfig()
